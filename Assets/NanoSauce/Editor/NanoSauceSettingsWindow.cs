2d7c2c9b276ac7c3e1634a9f4bd2bba8110795c7
Victor Nunes <vhndev@outlook.com>
2022-03-19 10:44:48 -0300

    Settings window created

---
+++ Assets/NanoSauce/Editor/NanoSauceSettingsWindow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace com.vhndev.nanosauce.setup.editor
+{
+    public class NanoSauceSettingsWindow : EditorWindow
+    {
+        [MenuItem("NanoSauce/Settings")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<NanoSauceSettingsWindow>();
+            window.titleContent = new GUIContent("NanoSauce Settings");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            DrawFBSettings();
+            GUILayout.Space(20);
+            
+            DrawGASettings();
+            GUILayout.Space(20);
+            
+            DrawApplyButton();
+        }
+
+        private void DrawGASettings()
+        {
+            EditorGUILayout.HelpBox("Game Analytics", MessageType.None);
+            EditorGUILayout.TextField("Android Game Key", "");
+            EditorGUILayout.TextField("Android Secret Key", "");
+            GUILayout.Space(10);
+            EditorGUILayout.TextField("iOS Game Key", "");
+            EditorGUILayout.TextField("iOS Secret Key","");
+        }
+        
+        private void DrawFBSettings()
+        {
+            EditorGUILayout.HelpBox("Facebook", MessageType.None);
+            EditorGUILayout.TextField("App ID", "");
+        }
+
+        private void DrawApplyButton()
+        {
+            if (GUILayout.Button("Apply"))
+                throw new System.NotImplementedException();
+        }
+    }
+}
