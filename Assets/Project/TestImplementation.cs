6a2064c17bb96081c1e3117370f681caec3509d2
Victor Nunes <vhndev@outlook.com>
2022-03-18 22:19:10 -0300

    Updated test implementation

---
+++ Assets/Project/TestImplementation.cs
@@ -0,0 +1,46 @@
+using com.vhndev.nanosauce;
+using UnityEngine;
+
+namespace HyperFactory
+{
+    public class TestImplementation : MonoBehaviour
+    {
+        #region Variables / Components
+        
+        
+        
+        #endregion
+
+        #region Events
+        
+        
+        
+        #endregion
+    
+        #region Monobehaviour
+        
+        
+        
+        #endregion
+
+        #region Methods
+
+        public void GameStart()
+        {
+            NanoSauceAnalytics.RegisterGameStart();
+        }
+        
+        public void GameWin()
+        {
+            NanoSauceAnalytics.RegisterGameWin();
+        }
+
+        public void GameLose()
+        {
+            NanoSauceAnalytics.RegisterGameLose();
+        }
+        
+        #endregion
+    }
+}
+
