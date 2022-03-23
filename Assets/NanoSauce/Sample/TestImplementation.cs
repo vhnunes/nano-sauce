812d87a02430b9df189422929ff64df247944bfb
Victor Nunes <vhndev@outlook.com>
2022-03-23 10:06:14 -0300

    Sample moved

---
+++ Assets/NanoSauce/Sample/TestImplementation.cs
@@ -0,0 +1,52 @@
+using com.vhndev.nanosauce.analytics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestImplementation : MonoBehaviour
+{
+    #region Variables / Components
+
+    [SerializeField] private Text text;
+        
+    #endregion
+
+    #region Events
+        
+        
+        
+    #endregion
+    
+    #region Monobehaviour
+
+    private void Start()
+    {
+        TestAB();
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void TestAB()
+    {
+        text.text = NanoSauceAnalytics.GetCurrentCohortID();
+    }
+        
+    public void GameStart()
+    {
+        NanoSauceAnalytics.RegisterGameStart();
+    }
+        
+    public void GameWin()
+    {
+        NanoSauceAnalytics.RegisterGameWin();
+    }
+
+    public void GameLose()
+    {
+        NanoSauceAnalytics.RegisterGameLose();
+    }
+        
+    #endregion
+}
+
